58a292efc851cd0066da812d9afb0b150f01895e
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-05 18:54:55 +0200

    Use jQuery to shorten js/script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-var span = $('span');
-span.each(function(index, element) {
-    if (index % 2 === 0) {
-        $(element).css('color', 'red');
-    }
-});
+var span = $('span:even').css('color', 'red');
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
